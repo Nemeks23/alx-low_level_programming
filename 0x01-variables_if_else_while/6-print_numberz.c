a0711198522357c98d63fa47ee33c91e5040850e
nemeks23 <nemeks23@gmail.com>
2022-07-02 14:51:05 +0000

    This commit is for task 6 under If, else and while in C Low level language

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always to 0 (Correct)
+*/
+
+int main(void)
+
+{
+int d;
+
+for (d = 48; d <= 57; d++)
+{
+putchar (d);
+}
+
+putchar ("\n");
+
+return (0);
+
+}
+
