479b2ed50d17fae5cc0792ca57df7b1d5aed68e4
nemeks23 <nemeks23@gmail.com>
2022-07-02 14:38:48 +0000

    This commit is for task 5 under If, else and while in C Low level language

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always to 0 (Correct)
+*/
+
+int main(void)
+
+{
+int numbers;
+
+for (numbers = 0 ; numbers < 10; numbers++)
+
+{
+printf("%numbers", numbers);
+}
+
+printf("\n");
+
+return (0);
+
+}
+
