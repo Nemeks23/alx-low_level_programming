4b35e1c4ec1136c5f9c98b20210b84bf32214a94
nemeks23 <nemeks23@gmail.com>
2022-07-02 01:40:32 +0000

    This commit is for task 2 under If, else and while in C Low level language

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success/correct)
+*/
+int main(void)
+
+{
+char nemeks;
+
+for (nemeks = 'a'; nemeks <= 'z' ; nemeks++)
+{
+putchar(nemeks);
+}
+
+putchar('\n');
+
+return (0);
+
+}
