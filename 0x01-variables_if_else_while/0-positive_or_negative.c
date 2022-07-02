a4f7c510b66c000b741934b6f02f691982f66b16
nemeks23 <nemeks23@gmail.com>
2022-07-02 00:54:37 +0000

    This commit is for task 0 Correction 2 under If, else and while in C Low level language

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,7 @@
 #include <time.h>
 /**
  *Main - Entry point
+ *
  *Return: Always 0 (Success/Correct)
 */
 
@@ -14,12 +15,12 @@ srand(time(0));
 
 n = rand() - RAND_MAX / 2;
 
-if (n==0)
+if (n = = 0)
 {
 printf("%i is zero\n", n);
 }
 
-else if (n<0)
+else if (n < 0)
 {
 printf("%i is negative\n", n);
 }
