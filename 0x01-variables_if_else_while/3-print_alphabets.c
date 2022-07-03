f55435163011b114ca113707840cc5c5cb23137c
nemeks23 <nemeks23@gmail.com>
2022-07-03 00:48:53 +0000

    This commit is for task 3 under If, else and while in C Low level language

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,10 @@
 int main(void)
 {
 char lower_case;
-char upper_case;
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar (lower_case);
-}
-
-for (upper_case = 'A'; uppercase <= 'Z'; upper_case++)
-{
-putchar (upper_case);
 }
 
 putchar ('\n');
