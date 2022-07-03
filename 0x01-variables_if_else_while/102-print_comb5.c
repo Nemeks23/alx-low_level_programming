5566045576f467c867a7a0b67db27beed398d0e0
nemeks23 <nemeks23@gmail.com>
2022-07-03 00:42:20 +0000

    This commit is for task 12 under If, else and while in C Low level language

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,15 +7,17 @@
 
 int main(void)
 {
+
 int num_1, num_2;
 
 for (num_1 = 0; num_1 <= 98; num_1++)
 {
 for (num_2 = num_1 + 1; num_2 <= 99; num_2++)
 {
+
 putchar ((num_1 / 10) + '0');
 putchar ((num_1 % 10) + '0');
-putchar (' ');
+/*putchar (' ');*/
 
 putchar ((num_2 / 10) + '0');
 putchar ((num_2 % 10) + '0');
@@ -28,6 +30,6 @@ putchar (' ');
 }
 }
 
-putchar ('\n);
+putchar ('\n');
 return (0);
 }
