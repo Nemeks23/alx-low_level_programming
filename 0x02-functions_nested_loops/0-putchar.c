745e10a7dedbee2f52aab716c549b9c93f818693
nemeks23 <nemeks23@gmail.com>
2022-07-05 23:06:30 +0000

    This commit is for task 0 under Functions and nested loops in C Low level language

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always to 0 (Successful)
+*/
+
+int main(void)
+{
+
+char nemeks = _putchar;
+
+putchar (nemeks);
+
+putchar ('\n');
+
+return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory is for Functions and Nested loops task in C programming Langusge
