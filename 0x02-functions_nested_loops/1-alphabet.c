241fe005d7bea73757aac7d9399369ed20d6f483
nemeks23 <nemeks23@gmail.com>
2022-07-06 00:13:40 +0000

    This commit is for task 1 under Functions and nested loops in C Low level language

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mian: A Function that prints the alphabet in Lower case
+ *
+ * Return: Always to 0 (Success)
+*/
+
+void rx_nemeks(void)
+{
+ char n;
+ for (n = 'a'; c <= 'z'; n++)
+       _putchar(n);
+ _putchar('\n');
+}
