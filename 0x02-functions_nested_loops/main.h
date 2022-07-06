2403b4e3deab1ef75f56fba1a5552e9244014064
nemeks23 <nemeks23@gmail.com>
2022-07-06 00:17:29 +0000

    This commit is for task 1 under Functions and nested loops in C Low level language

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
-void print_alphabeet(void);
-void print_alphabet_x10(void);
+void rx_nemeks(void);
+void rx_nemeks_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
