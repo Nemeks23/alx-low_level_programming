0bdd6040e42931ecbc141507ccb9b5698169e194
nemeks23 <nemeks23@gmail.com>
2022-07-06 00:47:29 +0000

    This commit is for task 2 under Functions and nested loops in C Low level language

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Printing lowercase alphabets 10 times.
+ *
+*/
+
+void print_alphabet_x10(void)
+
+{
+int i;
+int c;
+
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
