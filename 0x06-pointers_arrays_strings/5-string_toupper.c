f6c43b59f482f40d552b880d5ef0b8c0f8f375c4
Nemeks23 <93982453+Nemeks23@users.noreply.github.com>
2022-07-15 02:19:53 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - It changes all Lower case letter of a string to upper case
+ * @str: String
+ * Return: Returns char
+*/
 
+char *string_toupper(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+
+return (str);
+}
