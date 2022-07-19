aa6bd8df9cd4d5235d4cf7f543330b5444cf819a
Nemeks23 <93982453+Nemeks23@users.noreply.github.com>
2022-07-19 02:34:20 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - This function prints the consecutive characters of s1 thar are in s2
+ * @s: This is the Source string
+ * @accept: This is the searching string
+ * Return: New string
+*/
 
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+
+for (j = 0; *(s + j); j++)
+{
+for (i = 0; *(accept + i); i++)
+{
+if (*(s + j) == *(accept + i))
+break;
+}
+
+if (*(accept + i) == '\0')
+break;
+}
+return (j);
+
+}
