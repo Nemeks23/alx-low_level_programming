5010b98003806abeaa05874ce883238c5f6119f5
Nemeks23 <93982453+Nemeks23@users.noreply.github.com>
2022-07-19 22:38:08 +0100

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - This sets the value of a to a char.
+ * @s: Pointer to pointer.
+ * @to: pointer to char
+ *
+ * Return: void
+*/
+
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
