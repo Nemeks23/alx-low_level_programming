553811c119bd1f3c69fe6e9d74ed9398ee9477a0
Nemeks23 <93982453+Nemeks23@users.noreply.github.com>
2022-07-21 02:25:10 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,20 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns Length of string
+ * @s: Pointer to string
+ *
+ * Return: Length of string
+*/
 
+int _strlen_recursion(char *s)
+{
+if (*s)
+{
+return (1 + _strlen_recursion(s + 1));
+}
+
+else
+{
+return (0)
+}
+}
