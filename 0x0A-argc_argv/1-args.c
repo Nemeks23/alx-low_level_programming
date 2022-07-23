b82c96bd0a25e4df97d11876d03c8cc0bc08653a
Nemeks23 <93982453+Nemeks23@users.noreply.github.com>
2022-07-24 00:34:16 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the number of argument passed to it.
+ *
+ * @args: This is the argument count.
+ * @argv: This is the argument vector.
+ *
+ * Return: Returns to zero.
+*/
 
+int main(int args, char *argv[])
+{
+printf("%d\n", args - 1);
+(void)argv;
+return (0);
+}
