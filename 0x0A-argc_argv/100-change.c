1a3c231cca54a56e239ea48b2cfe8c8d77b07874
Nemeks23 <93982453+Nemeks23@users.noreply.github.com>
2022-07-24 02:14:48 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - This prints the amount of coins left
  * @args: Parameter entry.
@@ -25,7 +24,6 @@ if (c < 0)
 printf("0\n");
 return (0);
 }
-
 for (; c >= 0;)
 {
 if (c >= 25)
@@ -36,21 +34,15 @@ c -= 10;
 
 else if (c >= 5)
 c -= 5;
-
 else if (c >= 2)
 c -= 2;
-
 else if (c >= 1)
 c -= 1;
 
 else
 break;
-
 coins += 1;
 }
-
 printf("%d\n", coins);
-
 return (0);
-
 }
