b47d10a87113dae992a1107b08a280ac7689e467
Nemeks23 <93982453+Nemeks23@users.noreply.github.com>
2022-07-28 02:18:21 +0100

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,18 @@
-
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - free a 2D grid in alloc_grid.
+ * @grid: pointer to pointer.
+ * @height: height of grid.
+ *
+ * Return: void.
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
