d72c5cceee5344e2928f15a44265601ec5934cae
Nemeks23 <93982453+Nemeks23@users.noreply.github.com>
2022-08-03 01:15:11 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -26,16 +26,16 @@ _puts_recursion(s + 1);
  * Return: void
 */
 
-void _puts_recursion(char *s)
-{
-if (*s == '\0') /*Base case*/
-{
-_putchar('\n');
-return;
-}
-
-_putchar(*s);
-_puts_recursion(s + 1);
-
-}
+*void _puts_recursion(char *s)
+*{
+*if (*s == '\0') /*Base case*/
+*{
+*_putchar('\n');
+*return;
+*}
+*
+*_putchar(*s);
+*_puts_recursion(s + 1);
+*
+*}
 */
