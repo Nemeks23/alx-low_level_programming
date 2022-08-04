ceef9e5a51cb5acf750151f1953470ffd4e50fe8
Nemeks23 <93982453+Nemeks23@users.noreply.github.com>
2022-08-04 03:03:39 +0100

    tray

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,21 @@
+#include "function_pointers.h"
 
+/**
+ * array_iterator - Executes a function given as a.
+ * parameter On each element of an array.
+ * @array: Input integer array.
+ * @size: Size of the array.
+ * @action: Pointer to the function.
+ *
+ * Return: no return.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+
+if (array && action)
+{
+for (i = 0; i < size; i++)
+}
+action(array[i]);
+}
